634a4da39fd6839b784c9373e179e47b44366f32
scott <scott.a.benson189@gmail.com>
2021-03-20 18:37:23 -0500

    Updated Localhost port

--- src/apiFunctions.js
+++ src/apiFunctions.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = 'https://chat--app--api.herokuapp.com' || 'http://localhost:5000'
+const baseUrl = 'https://chat--app--api.herokuapp.com' || 'http://localhost:80'
 const getUserInfoEndpoint = '/user/'
 const getUsersEndpoint = '/users'
 const getPostsEndpoint = '/posts'
