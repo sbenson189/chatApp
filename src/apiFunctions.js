30fc0af3aa04ab89e172592b53e6b23a91e06807
scott <scott.a.benson189@gmail.com>
2021-03-20 18:20:48 -0500

    Update apiFunctions.js

--- src/apiFunctions.js
+++ src/apiFunctions.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = 'http://localhost:5000'
+const baseUrl = 'https://chat--app--api.herokuapp.com' || 'http://localhost:5000'
 const getUserInfoEndpoint = '/user/'
 const getUsersEndpoint = '/users'
 const getPostsEndpoint = '/posts'
